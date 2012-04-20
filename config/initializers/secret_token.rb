747483b861d80d23d6432a8452fca13b2dc60fe7
Steve Tipton <steve.tipton@employmentguide.com>
2012-04-20 11:27:37 -0400

    initial commit for new rails app: pennywise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pennywise::Application.config.secret_token = '970eefdb77a249f2aef5d0ab7b1593e127ab32eb03231c5db3695918a21638e384a6bc9a1a0e3ef49c73397759b59fa6f8af0688643ba5b67329e77c4786a538'

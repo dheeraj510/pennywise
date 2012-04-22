43ecd0f0b0bb6dbb1e9e8340941fab1b176e6edd
Steve Tipton <steve.tipton@employmentguide.com>
2012-04-22 19:27:31 -0400

    initial transaction_entry factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,4 +4,14 @@ FactoryGirl.define do
     name    "Capital One"
     type    { ["expense", "income"].sample }
   end
+
+  factory :transaction_entry do
+    item
+    type            { ["deposit", "autodraft", "atm", "check", "transfer", "checkcard"].sample }
+    check_number    { SecureRandom.random_number(1e4.to_i) }
+    memo            { "Payment 1/144" }
+    cleared         { false }
+    transacted_at   { Time.now }
+    cleared_at      nil
+  end
 end

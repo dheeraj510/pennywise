1283d8627aeccd547b4de5f2c9eeaae41e554b28
Steve Tipton <steve.tipton@employmentguide.com>
2012-04-22 21:01:17 -0400

    change type attribute

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,13 +1,13 @@
 FactoryGirl.define do
 
   factory :item do
-    name    { "Capital One" }
-    type    { ["expense", "income"].sample }
+    name          { "Capital One" }
+    item_type     { Item::ITEM_TYPES.sample }
   end
 
   factory :transaction_entry do
     item
-    type            { ["deposit", "autodraft", "atm", "check", "transfer", "checkcard"].sample }
+    entry_type      { ["deposit", "autodraft", "atm", "check", "transfer", "checkcard"].sample }
     check_number    { SecureRandom.random_number(1e4.to_i) }
     memo            { "Payment 1/144" }
     cleared         { false }

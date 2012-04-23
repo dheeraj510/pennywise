693eb4b236b809076cc68cd8899329ff69a7a84a
Steve Tipton <steve.tipton@employmentguide.com>
2012-04-22 21:01:48 -0400

    finish validation of Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ActiveRecord::Base
   has_many :transaction_entries
 
-  attr_accessible :name, :type
+  attr_accessible :name, :item_type
+
+  ITEM_TYPES = ["expense", "income"]
+
+  validates :name, :item_type, presence: true
+  validates :name, uniqueness: { scope: :item_type,
+                               message: "Item already exists" }
+  validates :item_type, inclusion: { in: ITEM_TYPES, message: "not valid" }
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,12 +7,30 @@ describe Item do
   describe "attributes" do
 
     it { should respond_to(:name) }
-    it { should respond_to(:type) }
+    it { should respond_to(:item_type) }
     it { should allow_mass_assignment_of(:name) }
-    it { should allow_mass_assignment_of(:type) }
+    it { should allow_mass_assignment_of(:item_type) }
   end
 
   describe "validations" do
+
+    subject{ FactoryGirl.create(:item)}
+
+    before(:each) do
+      @attrs = FactoryGirl.attributes_for(:item)
+    end
+
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:item_type) }
+
+    it { should allow_value('expense').for(:item_type) }
+    it { should allow_value('income').for(:item_type) }
+
+    it { should_not allow_value('payment').for(:item_type) }
+    it { should_not allow_value('123').for(:item_type) }
+
+    it { should validate_uniqueness_of(:name).scoped_to(:item_type).
+         with_message(/Item already exists/) }
   end
 
   describe "methods" do

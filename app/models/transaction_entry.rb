24447fa83a44b2631f551fdcd8190477e0cc036a
Steve Tipton <steve.tipton@employmentguide.com>
2012-04-22 21:16:06 -0400

    finish validation of TransactionEntry

--- app/models/transaction_entry.rb
+++ app/models/transaction_entry.rb
@@ -2,4 +2,10 @@ class TransactionEntry < ActiveRecord::Base
   belongs_to :item
 
   attr_accessible :check_number, :item_id, :memo, :transacted_at, :entry_type
+
+  ENTRY_TYPES = ["deposit", "autodraft", "atm", "check", "transfer", "checkcard"]
+
+  validates :item_id, :entry_type, :transacted_at, presence: true
+  validates :item_id, :check_number, numericality: true
+  validates :entry_type, inclusion: { in: ENTRY_TYPES, message: "not valid" }
 end
--- spec/models/transaction_entry_spec.rb
+++ spec/models/transaction_entry_spec.rb
@@ -23,6 +23,29 @@ describe TransactionEntry do
   end
 
   describe "validations" do
+
+    subject{ FactoryGirl.create(:transaction_entry)}
+
+    before(:each) do
+      @attrs = FactoryGirl.attributes_for(:transaction_entry)
+    end
+
+    it { should validate_presence_of(:item_id) }
+    it { should validate_presence_of(:entry_type) }
+    it { should validate_presence_of(:transacted_at) }
+
+    it { should validate_numericality_of(:item_id) }
+    it { should validate_numericality_of(:check_number) }
+
+    it { should allow_value('deposit').for(:entry_type) }
+    it { should allow_value('autodraft').for(:entry_type) }
+    it { should allow_value('atm').for(:entry_type) }
+    it { should allow_value('check').for(:entry_type) }
+    it { should allow_value('transfer').for(:entry_type) }
+    it { should allow_value('checkcard').for(:entry_type) }
+
+    it { should_not allow_value('payment').for(:entry_type) }
+    it { should_not allow_value('123').for(:entry_type) }
   end
 
   describe "methods" do

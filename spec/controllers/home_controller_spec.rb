9ecb758f5ee52d29400b23bd04fa28cd6b0aad80
Steve Tipton <steve.tipton@employmentguide.com>
2012-04-24 23:15:21 -0400

    instantiate @items

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe HomeController do
+  render_views
+
+  before(:each) do
+    @item = FactoryGirl.create(:item)
+  end
 
   describe "GET 'index'" do
 
@@ -10,6 +15,12 @@ describe HomeController do
 
     it "should be successful" do
       response.should be_success
+    end
+
+    it "should assign @items" do
+      assigns(:items).should be_an(Array)
+      assigns(:items).should_not be_empty
+      assigns(:items).sample.should be_an(Item)
     end
   end
 end

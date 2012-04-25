a34f2ad0f0303ef4bd30962fff2a4b041a4c917e
Steve Tipton <steve.tipton@employmentguide.com>
2012-04-24 23:23:56 -0400

    instantiate @transactions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
 
   def index
     @items = Item.all
+    @transactions = TransactionEntry.all
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -5,6 +5,7 @@ describe HomeController do
 
   before(:each) do
     @item = FactoryGirl.create(:item)
+    @transaction_entry = FactoryGirl.create(:transaction_entry, item: @item)
   end
 
   describe "GET 'index'" do
@@ -21,6 +22,13 @@ describe HomeController do
       assigns(:items).should be_an(Array)
       assigns(:items).should_not be_empty
       assigns(:items).sample.should be_an(Item)
+    end
+
+    it "should assign @transactions" do
+      assigns(:transactions).should be_an(Array)
+      assigns(:transactions).should_not be_empty
+      assigns(:transactions).sample.should be_a(TransactionEntry)
+      assigns(:transactions).sample.item_id.should eq @item.id
     end
   end
 end
